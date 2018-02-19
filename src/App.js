4efde2924666550c193246473d4d8b9fc74d8581
Denis Michailov <kamrad.reg@gmail.com>
2018-02-19 21:39:40 +1000

    base ReactRedux app with Router

--- src/App.js
+++ src/App.js
@@ -1,13 +1,24 @@
 import React, { Component } from 'react';
+import { Route, Redirect } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 import Incrementer from './components/Incrementer';
+import About from './components/About';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        Application
-        <Incrementer/>
+        <div className="header">
+          <NavLink to='/'>Home</NavLink>
+          <NavLink to='/incrementer'>Incrementer</NavLink>
+          <NavLink to='/about'>About</NavLink>
+        </div>
+        <div className="workspace">
+          <Route exact path="/" render={props => <Redirect to="incrementer" />} />
+          <Route exact path="/incrementer" component={Incrementer} />
+          <Route exact path="/about" component={About} />
+        </div>
       </div>
     );
   }
---
+++ src/components/About.jsx
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+class About extends Component {
+
+  render() {
+    return (
+      <div className="About">
+        Just About page
+      </div>
+    );
+  }
+
+}
+
+export default About;
